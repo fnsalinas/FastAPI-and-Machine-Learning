9e7714a441b6549133daa0109697adf0b1804f82
Fabio Salinas <fabio.salinas1982@gmail.com>
2022-12-22 14:49:52 -0500

    Thu Dec 22 14:49:52 -05 2022 - Pushing changes to GitHub

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -12,3 +15,17 @@ def read_item(item_id: int):
         "item_id": item_id,
         "message": "Hello World"
         }
+
+class Item(BaseModel):
+    name: str
+    price: float
+    tags: List[str] = []
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
+
+@app.get("/generate")
+def generate_image(prompt: str):
+    return {"prompt": prompt}
+
