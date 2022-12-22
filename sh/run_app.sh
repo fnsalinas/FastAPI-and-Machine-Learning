2f5389097250aadc977cd17614d8198fcce5acdc
Fabio Salinas <fabio.salinas1982@gmail.com>
2022-12-22 15:33:44 -0500

    Thu Dec 22 15:33:44 -05 2022 - Pushing changes to GitHub

--- sh/run_app.sh
+++ sh/run_app.sh
@@ -1,4 +1,7 @@
 #!bin/bash
 
+echo "Starting FastAPI and Machine Learning";
 cd ~/workspace/fsalinas/FastAPI-and-Machine-Learning;
+
+echo "Starting FastAPI and Machine Learning";
 exec uvicorn main:app --reload;
